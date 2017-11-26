d917afdb7f855817bffa6c4cb012e177487c0a5f
clarzou <clairezunda@gmail.com>
2017-11-26 15:17:45 +0100

    basic authentification with device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Uncomment the next if you want to make the homepage accessible to only registered users
+  # before_action :authenticate_user!
 end
