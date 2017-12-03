d8588bb402875532460cf9074c1f7b7918ee1a72
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-12-03 12:45:14 +0100

    feat(charges): use new layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'welcome/index'
   as :user do 
     get 'profile/edit' => 'devise/registrations#edit'
   end
-
   root 'welcome#index'
-  devise_for :users
   resources :charges
 end
