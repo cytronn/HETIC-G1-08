dd70eb7f4a80cfbb101212c7c11a5fa6ca7cf882
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-12-03 11:52:20 +0100

    fix(charges): correct some issues on prices

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,9 +1,10 @@
 class ChargesController < ApplicationController
   def new
+    @amount = 510
   end
   
   def create
-    @amount = 500
+    @amount = 510
   
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -13,12 +14,12 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+      :description => 'Yuumm',
+      :currency    => 'eur'
     )
   
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to new_charge_path, amount: @amount
   end
 end
