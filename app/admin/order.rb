c63a00e95085d08c5f7c1005b85bf5791cc1c8f7
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-12-14 09:14:10 +0100

    feat(admin): link dish and order models

---
+++ app/admin/order.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Order do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  permit_params do
+    permitted = [:permitted, :attributes]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  permit_params :name, :note, :quantity, :status, :slug, :amount
+
+  before_action :only => [:show, :edit, :update, :delete] do
+    @order = Order.find_by!(slug: params[:id])
+  end
+
+end
