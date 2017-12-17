f60feafa630720955d12ebeff72c8b15e160d13b
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-12-17 17:40:16 +0100

    fix(mailer): typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Mailing
   config.action_mailer.default_url_options = { :host => 'https://hetic-g1-8.herokuapp.com/' }
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME']
+    :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'yourdomain.com',
     :address => 'smtp.sendgrid.net',
